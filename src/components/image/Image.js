53eccd5a5c1d56224b4b3b4da8edf7ef86daa038
TanTN <ttan6596@gmail.com>
2023-04-05 20:57:42 +0700

    add

---
+++ src/components/image/Image.js
@@ -0,0 +1,26 @@
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import { useState, forwardRef } from 'react';
+import styles from './image.module.scss'
+import images from '~/assest/images';
+function Image({ alt, src, className, fallBack: customFallBack = images.noImage,...props }, ref) {
+    const [fallBack, setFallBack] = useState('');
+    const handleError = () =>{
+        setFallBack(customFallBack)
+    }
+    return <img 
+    className={classNames(styles.wrapper,className)} 
+    alt={alt} 
+    src={fallBack || src} 
+    {...props} ref={ref} 
+    onError={handleError}
+    />;
+}
+Image.propTypes = {
+    alt:PropTypes.string,
+    src:PropTypes.string,
+    className:PropTypes.string,
+    fallBack:PropTypes.string,
+}
+
+export default forwardRef(Image);
